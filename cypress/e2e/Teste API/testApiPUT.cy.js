0abd3eb01cfd5f425b10fb09f700f1a99d207e95
Leonardo Pompilio <leopompiliobizarro@gmail.com>
2024-10-14 10:56:23 -0300

    API PUT

--- cypress/e2e/Teste API/testApiPUT.cy.js
+++ cypress/e2e/Teste API/testApiPUT.cy.js
@@ -3,17 +3,15 @@
 describe('Teste API PUT', () => {
 
     it('Rodar a API', () => {
-      cy.api('PUT','https://www.advantageonlineshopping.com/catalog/api/v1/product/image/{974877526}/{source}/{545195}',){
-      }
+      cy.api({
+        method: "PUT",
+        url: "https://www.advantageonlineshopping.com/catalog/api/v1/product/image/974877526/source/3683D1?product_id=20"
       })
         .then((response) => {
           expect(response.status).to.equal(200)
   
      })
    })
+  })
 
-
-   //não consegui fazer o teste dessa API, todas as requests que tentei mesmo no SWAGGER retornaram erro mesmo após fazer o login e pegar o Bearer Token,
-   //a mesma retorna como se o token estivesse errado, com isso não consegui fazer a autenticação para poder fazer a request PUT.
-   //irei mandar um documento separado mostrando o erro com a chamada da API.
-  
+  //não consegui pegar onde fica o "source" para realizar a chamada PUT.
